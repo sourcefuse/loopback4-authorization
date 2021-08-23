37c373f19723a3c33acd67990c61791785f28d98
Akshat  Dubey <akshat.dubey@sourcefuse.com>
2021-08-23 15:38:12 +0530

    feat(core): add permission specifying decorators
    GH-27

--- src/decorators/authorize.decorator.ts
+++ src/decorators/authorize.decorator.ts
@@ -1,9 +1,23 @@
-import {MethodDecoratorFactory} from '@loopback/core';
+import {
+  MetadataInspector,
+  MethodDecoratorFactory,
+  Reflector,
+} from '@loopback/core';
 import {AuthorizationMetadata} from '../types';
 import {AUTHORIZATION_METADATA_ACCESSOR} from '../keys';
+import {specPreprocessor} from './spec-preprocessor';
+import {OperationObject} from '@loopback/rest';
+
+type OperationMeta = {
+  verb: string;
+  path: string;
+  spec: OperationObject;
+};
+
+const OAI3KEY_METHODS = 'openapi-v3:methods';
 
 export function authorize(metadata: AuthorizationMetadata) {
-  return MethodDecoratorFactory.createDecorator<AuthorizationMetadata>(
+  const authorizedecorator = MethodDecoratorFactory.createDecorator<AuthorizationMetadata>(
     AUTHORIZATION_METADATA_ACCESSOR,
     {
       permissions: metadata.permissions || [],
@@ -11,4 +25,23 @@ export function authorize(metadata: AuthorizationMetadata) {
       isCasbinPolicy: metadata.isCasbinPolicy ?? false,
     },
   );
+  const authorizationWithMetadata = <T>(
+    target: Object,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<T>,
+  ) => {
+    const meta: OperationMeta | undefined = MetadataInspector.getMethodMetadata(
+      OAI3KEY_METHODS,
+      target,
+      propertyKey,
+    );
+    if (meta) {
+      meta.spec = specPreprocessor(target, propertyKey, metadata, meta.spec);
+      Reflector.deleteMetadata(OAI3KEY_METHODS, target, propertyKey);
+      Reflector.defineMetadata(OAI3KEY_METHODS, meta, target, propertyKey);
+      authorizedecorator(target, propertyKey, descriptor);
+    }
+  };
+
+  return authorizationWithMetadata;
 }
