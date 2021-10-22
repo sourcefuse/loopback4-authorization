6878d77563fa07630506e72300d79c1e644c0bbb
akshatdubeysf <77672713+akshatdubeysf@users.noreply.github.com>
2021-10-22 14:20:05 +0530

    feat(core): add permission specifying decorators (#28)
    GH-27

---
+++ src/decorators/spec-preprocessor.ts
@@ -0,0 +1,36 @@
+import {OperationObject} from 'openapi3-ts';
+import {AuthorizationMetadata} from '../types';
+
+const defaultResponse = (ctor: {name: string}, op: string) => ({
+  '200': {
+    description: `Return value of ${ctor.name}.${op}`,
+  },
+});
+
+export const specPreprocessor = (
+  target: Object,
+  propertyKey: string,
+  authorizations: AuthorizationMetadata,
+  spec?: OperationObject,
+) => {
+  let desc = spec?.description ?? '';
+  if (authorizations?.permissions && authorizations?.permissions.length > 0) {
+    authorizations.permissions
+      .filter((permission: string) => permission.trim() !== '*')
+      .forEach((permission, i) => {
+        if (i === 0) {
+          desc += `\n\n| Permissions |\n| ------- |\n`;
+        }
+        desc += `| ${permission}   |\n`;
+      });
+  }
+  if (spec) {
+    spec.description = desc;
+  } else {
+    spec = {
+      description: desc,
+      responses: defaultResponse(target.constructor, propertyKey),
+    } as OperationObject;
+  }
+  return spec;
+};
