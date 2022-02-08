33e8a50af2a2ca0e6a4080e1404f38fe82be02b5
Rohit Vishwakarma <rohit.vishwakarma@sourcefuse.com>
2022-02-08 23:05:27 +0530

    fix(provider): fix(provider): authorization action gives 404 for route not found
    GH-35

--- src/__tests__/unit/authorization-action.provider.unit.ts
+++ src/__tests__/unit/authorization-action.provider.unit.ts
@@ -8,6 +8,7 @@ import {
 import {Request} from 'express';
 import {AuthorizeActionProvider} from '../../providers';
 import {mockUser} from './data/mock-user';
+import {Context} from '@loopback/core';
 
 const mockPermissions: AuthorizationMetadata = {
   permissions: ['ViewTodo'],
@@ -16,27 +17,41 @@ const mockPermissions: AuthorizationMetadata = {
 describe('AuthorizeActionProvider', () => {
   it('should return true if user has required permissions', async () => {
     const metadataGetterStub = sinon.stub().resolves(mockPermissions);
-    const action = new AuthorizeActionProvider(metadataGetterStub, []).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      [],
+      getRequestContext(),
+    ).value();
     const result = await action(mockUser.permissions);
     expect(result).to.be.true();
   });
   it('should return true for all users if resource has "*" as the first permissions', async () => {
     const metadataGetterStub = sinon.stub().resolves({permissions: ['*']});
-    const action = new AuthorizeActionProvider(metadataGetterStub, []).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      [],
+      getRequestContext(),
+    ).value();
     const result = await action([]);
     expect(result).to.be.true();
   });
   it('should return false if user does not have required permissions', async () => {
     const metadataGetterStub = sinon.stub().resolves(mockPermissions);
-    const action = new AuthorizeActionProvider(metadataGetterStub, []).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      [],
+      getRequestContext(),
+    ).value();
     const result = await action([]);
     expect(result).to.be.false();
   });
   it('should return true if requested resource is in always allowed list', async () => {
     const metadataGetterStub = sinon.stub().resolves(mockPermissions);
-    const action = new AuthorizeActionProvider(metadataGetterStub, [
-      '/always/allowed',
-    ]).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      ['/always/allowed'],
+      getRequestContext(),
+    ).value();
     const mockRequest = givenRequest({
       url: '/always/allowed',
       payload: {},
@@ -46,9 +61,11 @@ describe('AuthorizeActionProvider', () => {
   });
   it('should return true if requested resource has a parent path in always allowed list', async () => {
     const metadataGetterStub = sinon.stub().resolves(mockPermissions);
-    const action = new AuthorizeActionProvider(metadataGetterStub, [
-      '/always/allowed',
-    ]).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      ['/always/allowed'],
+      getRequestContext(),
+    ).value();
     const mockRequest = givenRequest({
       url: '/always/allowed/child',
       payload: {},
@@ -58,9 +75,11 @@ describe('AuthorizeActionProvider', () => {
   });
   it('should return false if requested resource is not in always allowed list, and user does not have required permissions', async () => {
     const metadataGetterStub = sinon.stub().resolves(mockPermissions);
-    const action = new AuthorizeActionProvider(metadataGetterStub, [
-      '/not/always/allowed',
-    ]).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      ['/not/always/allowed'],
+      getRequestContext(),
+    ).value();
     const mockRequest = givenRequest({
       url: '/always/allowed',
       payload: {},
@@ -69,13 +88,36 @@ describe('AuthorizeActionProvider', () => {
     expect(result).to.be.false();
   });
   it('should return false if resource has no attached metadata', async () => {
+    const requestContext = getRequestContext();
+    requestContext.get.resolves('');
+
     const metadataGetterStub = sinon.stub().resolves();
-    const action = new AuthorizeActionProvider(metadataGetterStub, []).value();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      [],
+      requestContext,
+    ).value();
     const result = await action(mockUser.permissions);
     expect(result).to.be.false();
   });
 
+  it('should throw 404 if non existing route is hit', async () => {
+    const requestContext = getRequestContext();
+    requestContext.get.throws();
+    const metadataGetterStub = sinon.stub().resolves();
+    const action = new AuthorizeActionProvider(
+      metadataGetterStub,
+      [],
+      requestContext,
+    ).value();
+    const result = await action(mockUser.permissions).catch(err => err);
+    expect(result).be.instanceOf(Error);
+    expect(result).to.have.property('message').which.eql('Not Found');
+  });
   function givenRequest(options?: ShotRequestOptions): Request {
     return stubExpressContext(options).request;
+  }
+  function getRequestContext(): sinon.SinonStubbedInstance<Context> {
+    return sinon.createStubInstance(Context);
   }
 });
