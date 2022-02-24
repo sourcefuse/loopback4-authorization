dc4f10a97b623c78c06f9aadc44ae23344936aea
Rohit Vishwakarma <rohit.vishwakarma@sourcefuse.com>
2022-02-24 12:07:11 +0530

    fix(component): fix(component): fix authorize permission missing in path description
    GH-40

---
+++ src/enhancer/spec-description-enhancer.ts
@@ -0,0 +1,43 @@
+import {
+  Application,
+  CoreBindings,
+  inject,
+  injectable,
+  MetadataInspector,
+} from '@loopback/core';
+import {
+  asSpecEnhancer,
+  OASEnhancer,
+  OpenAPIObject,
+  OpenApiSpec,
+  RestEndpoint,
+} from '@loopback/rest';
+
+@injectable(asSpecEnhancer)
+export class DescSpecEnhancer implements OASEnhancer {
+  @inject(CoreBindings.APPLICATION_INSTANCE) private readonly app: Application;
+  name = 'info';
+  modifySpec(spec: OpenAPIObject): OpenApiSpec {
+    for (const controller of this.app.find(`${CoreBindings.CONTROLLERS}.*`)) {
+      const ctor = controller.valueConstructor;
+      if (ctor) {
+        const endpoints = MetadataInspector.getAllMethodMetadata<RestEndpoint>(
+          'openapi-v3:methods',
+          ctor.prototype,
+        );
+        for (const route in endpoints) {
+          const routeData = endpoints[route];
+          if (
+            routeData?.spec?.description &&
+            !spec.paths[routeData.path][routeData.verb].description
+          ) {
+            spec.paths[routeData.path][routeData.verb].description =
+              routeData.spec.description;
+          }
+        }
+      }
+    }
+
+    return spec;
+  }
+}
